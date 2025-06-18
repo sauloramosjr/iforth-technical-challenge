72568fda381d763874767711b30fe2c0732cde29
Saulo Ramos <saulo.junior@useall.com.br>
2025-06-18 18:07:39 -0300

    feat: add middleware for authentication and validations

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,16 +2,24 @@ import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 import { verifyToken } from './lib/auth';
 
+
 export function middleware(request: NextRequest) {
   const apiKey = request.headers.get('x-api-key');
 
-//   if (!apiKey || !verifyToken(apiKey)) {
-//     return returnUnauthorized(); 
-//   }
+  const path = request.nextUrl.pathname
+ 
+  // if(path.includes('dashboard')){
+
+  //   if(!apiKey || !verifyToken(apiKey)){
+  //     return Unauthorized()
+  //   }
+
+  // }
+  
 
   return NextResponse.next();
 }
 
-const returnUnauthorized = () => {
+const Unauthorized = () => {
   return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
 };
